cfb6e10213eca21bd38ff942471a2986810d36bd
antlypls <antlypls@gmail.com>
2010-06-13 21:51:47 +0400

    initial commit

---
+++ ClipboardWatcher/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ClipboardWatcher
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      StopMonitor();
+
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.lbxWords = new System.Windows.Forms.ListBox();
+      this.btnStart = new System.Windows.Forms.Button();
+      this.btnStop = new System.Windows.Forms.Button();
+      this.btnSave = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // lbxWords
+      // 
+      this.lbxWords.FormattingEnabled = true;
+      this.lbxWords.HorizontalScrollbar = true;
+      this.lbxWords.Location = new System.Drawing.Point(12, 12);
+      this.lbxWords.Name = "lbxWords";
+      this.lbxWords.ScrollAlwaysVisible = true;
+      this.lbxWords.Size = new System.Drawing.Size(452, 277);
+      this.lbxWords.TabIndex = 0;
+      // 
+      // btnStart
+      // 
+      this.btnStart.Location = new System.Drawing.Point(12, 295);
+      this.btnStart.Name = "btnStart";
+      this.btnStart.Size = new System.Drawing.Size(75, 23);
+      this.btnStart.TabIndex = 1;
+      this.btnStart.Text = "Start";
+      this.btnStart.UseVisualStyleBackColor = true;
+      this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+      // 
+      // btnStop
+      // 
+      this.btnStop.Enabled = false;
+      this.btnStop.Location = new System.Drawing.Point(389, 295);
+      this.btnStop.Name = "btnStop";
+      this.btnStop.Size = new System.Drawing.Size(75, 23);
+      this.btnStop.TabIndex = 2;
+      this.btnStop.Text = "Stop";
+      this.btnStop.UseVisualStyleBackColor = true;
+      this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+      // 
+      // btnSave
+      // 
+      this.btnSave.Location = new System.Drawing.Point(12, 361);
+      this.btnSave.Name = "btnSave";
+      this.btnSave.Size = new System.Drawing.Size(75, 23);
+      this.btnSave.TabIndex = 3;
+      this.btnSave.Text = "Save to CP";
+      this.btnSave.UseVisualStyleBackColor = true;
+      this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(476, 447);
+      this.Controls.Add(this.btnSave);
+      this.Controls.Add(this.btnStop);
+      this.Controls.Add(this.btnStart);
+      this.Controls.Add(this.lbxWords);
+      this.Name = "Form1";
+      this.Text = "ClipboardWatcher";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox lbxWords;
+    private System.Windows.Forms.Button btnStart;
+    private System.Windows.Forms.Button btnStop;
+    private System.Windows.Forms.Button btnSave;
+  }
+}
+
---
+++ ClipboardWatcher/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ClipboardWatcher
+{
+  public partial class Form1 : Form
+  {
+    private IntPtr nextClipboardViewer;
+    private bool _isStart = false;
+
+    public Form1()
+    {
+      InitializeComponent();
+      lbxWords.Items.Clear();
+    }
+
+    void StartMonitor()
+    {
+      if (_isStart)
+      {
+        return;
+      }
+
+      Clipboard.Clear();
+      nextClipboardViewer = (IntPtr)SetClipboardViewer((int)this.Handle);
+      _isStart = true;
+    }
+
+    void StopMonitor()
+    {
+      if (!_isStart)
+      {
+        return;
+      }
+
+      ChangeClipboardChain(this.Handle, nextClipboardViewer);
+      _isStart = false;
+    }
+
+    string ProcessWord(string word)
+    {
+      var trimmed = word.Trim(new char[] {' ', '.', ','} );
+      return trimmed.ToLower();
+    }
+
+    private void DisplayClipboardData()
+    {
+      IDataObject data = Clipboard.GetDataObject();
+      if (data.GetDataPresent(DataFormats.Text))
+      {
+        string cpdata = data.GetData(DataFormats.Text).ToString();
+
+        if (lbxWords.Items.Count == 0 || (string) lbxWords.Items[lbxWords.Items.Count - 1] != cpdata)
+        {
+          lbxWords.Items.Add(cpdata);
+        }
+      }
+    }
+
+    [DllImport("User32.dll")]
+    protected static extern int SetClipboardViewer(int hWndNewViewer);
+
+    [DllImport("User32.dll", CharSet = CharSet.Auto)]
+    public static extern bool ChangeClipboardChain(IntPtr hWndRemove, IntPtr hWndNewNext);
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+    protected override void WndProc(ref System.Windows.Forms.Message message)
+    {
+      const int WM_DRAWCLIPBOARD = 0x308;
+      const int WM_CHANGECBCHAIN = 0x030D;
+
+      switch (message.Msg)
+      {
+        case WM_DRAWCLIPBOARD:
+          DisplayClipboardData();
+          SendMessage(nextClipboardViewer, message.Msg, message.WParam, message.LParam);
+          break;
+
+        case WM_CHANGECBCHAIN:
+          if (message.WParam == nextClipboardViewer)
+          {
+            nextClipboardViewer = message.LParam;
+          }
+          else
+          {
+            SendMessage(nextClipboardViewer, message.Msg, message.WParam, message.LParam);
+          }
+          break;
+
+        default:
+          base.WndProc(ref message);
+          break;
+      }
+    }
+
+    private void btnStart_Click(object sender, EventArgs e)
+    {
+      StartMonitor();
+      btnStart.Enabled = false;
+      btnStop.Enabled = true;
+    }
+
+    private void btnStop_Click(object sender, EventArgs e)
+    {
+      StopMonitor();
+      btnStart.Enabled = true;
+      btnStop.Enabled = false;
+    }
+
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+      var builder = new StringBuilder();
+      foreach (string str in lbxWords.Items)
+      {
+        builder.AppendLine(ProcessWord(str));
+      }
+
+      Clipboard.SetDataObject(builder.ToString(), true);
+    }
+
+  }
+}
